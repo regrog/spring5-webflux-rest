ae7c1773f7e68c3d1734666d691faf77697fc926
Andrea Vencato <avencato@sanmarcoinformatica.it>
2020-05-01 15:25:30 +0200

    vendor implementation

---
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webfluxrest.controllers;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CategoryController {
+    private CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    public Flux<Category> list(){
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    public Mono<Category> getById(@PathVariable String id){
+        return categoryRepository.findById(id);
+    }
+}
